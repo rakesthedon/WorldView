1e9455af4c4b414ceb1117ae8a4e81ebb19e177a
Yannick Jacques <18355962+rakesthedon@users.noreply.github.com>
2024-05-28 19:35:12 -0400

    Creating Xcode project

---
+++ WorldView/WorldViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldViewApp.swift
+//  WorldView
+//
+//  Created by Yannick Jacques on 2024-05-28.
+//
+
+import SwiftUI
+
+@main
+struct WorldViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
